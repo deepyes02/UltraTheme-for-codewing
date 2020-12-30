e8d10e1db21303843bb8a4c1e9468a02f400f26a
deepyes02 <deepyes@outlook.com>
2020-12-30 22:27:28 +0545

    adding more admin styles

--- includes/templates/ultra_setting_page.php
+++ includes/templates/ultra_setting_page.php
@@ -1,1 +1,14 @@
-<h3>Here you can manage your homepage intro section that appears below the CTA.</h3>
+<?php 
+/**
+ * Main settings page functions
+ */
+?>
+
+<h3>Manage General Fields for managing on your homepage</h3>
+<p>Customize Sidebar options</p>
+
+<form method="post" action="">
+    <?php
+    settings_fields('ultra_setting_group');
+    ?>
+</form>
